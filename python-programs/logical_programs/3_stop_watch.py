7593a0c9f0efaf9b09b2914ba048f30afa53f7cc
Jayakrishnaa08 <javnd1234@gmail.com>
2025-02-22 10:52:18 +0530

    [kalyan] add: Added 1_user_input.py basic core program

--- python-programs/logical_programs/3_stop_watch.py
+++ python-programs/logical_programs/3_stop_watch.py
@@ -1,7 +1,13 @@
 import time
 
+def stopwatch():
+    input("Press Enter to start the stopwatch...")  #input for start
+    start_time = time.time()
 
-def stop_watch():
-    print("Start stopwatch")
-    start_time=time.time()
-    end_time=time.time()
+    input("Press Enter to stop the stopwatch...")  #input for stop
+    end_time = time.time()
+
+    duration = end_time - start_time
+    print(f"Elapsed time: {duration:.2f} seconds")
+
+stopwatch()
