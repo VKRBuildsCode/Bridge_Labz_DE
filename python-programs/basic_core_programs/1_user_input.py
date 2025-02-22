bb77d56c73f5e4e3d3a326278e18fcec4747cf48
Jayakrishnaa08 <javnd1234@gmail.com>
2025-02-22 10:53:56 +0530

    [kalyan] add: Added 1_user_input.py basic core program

--- python-programs/basic_core_programs/1_user_input.py
+++ python-programs/basic_core_programs/1_user_input.py
@@ -1,7 +1,4 @@
 def print_user_input():
-    """
-    This function is used to take username as input from user, and greet user
-    """
     user_name = input("Please enter your username : ")
     if len(user_name)>2:print(f"👋 Hello {user_name}, How are you? ☺️")
     else:
