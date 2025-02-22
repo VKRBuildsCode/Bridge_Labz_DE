77de24433f307d7fdb828337360d5b435ac1187a
Jayakrishnaa08 <javnd1234@gmail.com>
2025-02-22 11:14:15 +0530

    [kalyan] add: 2_sum_of_three_integers functional program

--- python-programs/functional_programs/2_sum_of_three_integers.py
+++ python-programs/functional_programs/2_sum_of_three_integers.py
@@ -1,7 +1,7 @@
+#Function for finding unique triplets that computes to zero
 def triplets(arr, n):
     triplets = []
     count = 0
-
     for i in range(n - 2):
         for j in range(i + 1, n - 1):
             for k in range(j + 1, n):
@@ -10,12 +10,10 @@ def triplets(arr, n):
                     if triplet not in triplets:  # Ensure distinct triplets
                         triplets.append(triplet)
                         count += 1
-
     print("Number of distinct triplets:", count)
     print("Distinct triplets:", triplets)
-
-
 n = int(input("Enter number of elements: "))
 arr = list(map(int, input("Enter elements separated by space: ").split()))
 
+#Calling th function here
 triplets(arr, n)
