ca8b7060aee23e395ede4a709bc444d1196843af
Jayakrishnaa08 <javnd1234@gmail.com>
2025-02-22 11:15:06 +0530

    [kalyan] add: 3_distance functional program

--- python-programs/functional_programs/3_distance.py
+++ python-programs/functional_programs/3_distance.py
@@ -1,7 +1,9 @@
 from math import sqrt
+
+#Finding distance between two points
 def calculate_euclidean_distance(x,y):
     euclidean_distance = sqrt(pow(x, 2) + pow(y, 2))
     return  euclidean_distance
 x=int(input("Enter the coordinate point of x axis :"))
 y=int(input("Enter the coordinate point of y axis :"))
-print(f"The euclidean distance from the point({x},{y}) to the origin(0,0) is {calculate_euclidean_distance(x,y)}")
+print(f" The euclidean distance from the point({x},{y}) to the origin(0,0) is {calculate_euclidean_distance(x,y)}")
