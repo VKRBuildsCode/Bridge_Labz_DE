dcb726a8925634ee03a912cbb18c3e8ffb06b26b
Jayakrishnaa08 <javnd1234@gmail.com>
2025-02-22 11:46:53 +0530

    [kalyan] add: Added UC1_Check_Attendance file

--- python-programs/employee_wage_problems/UC1_Check_Attendance.py
+++ python-programs/employee_wage_problems/UC1_Check_Attendance.py
@@ -1,5 +1,6 @@
 from random import randint
 
+#Function used to check attendance
 def check_attendance():
     attendance = randint(0, 2)
     if attendance == 0:
@@ -9,3 +10,4 @@ def check_attendance():
     else:
         print("Part-time Employee is Present")
     return attendance
+check_attendance()
